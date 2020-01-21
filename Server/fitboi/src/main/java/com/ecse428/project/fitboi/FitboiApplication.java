bff90cd95f7d330bab26db03c9cda5f0a987be79
He Qian Wang <heqianw@gmail.com>
2020-01-21 18:22:07 -0500

    Initial Commit with functional Spring Boot Web Server

---
+++ Server/fitboi/src/main/java/com/ecse428/project/fitboi/FitboiApplication.java
@@ -0,0 +1,13 @@
+package com.ecse428.project.fitboi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitboiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitboiApplication.class, args);
+	}
+
+}
