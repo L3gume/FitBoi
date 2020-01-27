874661d3bb76d04c75842d391b6afea445b71350
Brendan Kellam <bshizzle1234@gmail.com>
2020-01-26 22:37:44 -0500

    Added gherkin support to project along with high priority gherkin feature files (#6)
    * Added cucumber config to gradle build
    
    * Added example gherkin file 'is it friday yet'
    
    * Added all gherkin feature files and empty step definitions to project

---
+++ App/app/src/test/java/com/example/fitboi/cucumber/steps/ID003_User_Logs_A_Food_Item.java
@@ -0,0 +1,4 @@
+package com.example.fitboi.cucumber.steps;
+
+public class ID003_User_Logs_A_Food_Item {
+}
---
+++ App/app/src/test/java/com/example/fitboi/cucumber/steps/ID009_User_Adds_Custom_Food_Item_To_Database.java
@@ -0,0 +1,4 @@
+package com.example.fitboi.cucumber.steps;
+
+public class ID009_User_Adds_Custom_Food_Item_To_Database {
+}
