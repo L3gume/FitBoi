a7f67cd3462aca67802fe4c69d9b7acb66cab318
Anudruth Manjunath <anudruth@users.noreply.github.com>
2020-02-05 17:49:29 -0500

    User/anudruth/15 model (#48)
    * Added .vscode and project settings to gitignore
    
    * Auto generated model classes. Errors fixed and successful compile
    
    * Changed macros to float
    
    * Removed interdependecies between models
    
    * removed unused variable
    
    * Meals class renamed to Meal and portionSize changed to float. Based on comments from PR
    
    * Added umple files to repo
    
    Co-authored-by: He Qian Wang <he.q.wang@mail.mcgill.ca>

---
+++ Server/fitboi/src/main/java/com/ecse428/project/fitboi/model/FoodItem.java
@@ -0,0 +1,112 @@
+package com.ecse428.project.fitboi.model;
+
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.29.1.4753.5a97eca04 modeling language!*/
+
+
+
+// line 55 "model.ump"
+// line 104 "model.ump"
+public class FoodItem
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //FoodItem Attributes
+  private String name;
+  private int calories;
+  private float portionSize;
+
+  //FoodItem Associations
+  private MacroDistribution macroDistribution;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public FoodItem(String aName, int aCalories, float aPortionSize, MacroDistribution aMacroDistribution)
+  {
+    name = aName;
+    calories = aCalories;
+    portionSize = aPortionSize;
+    if (aMacroDistribution == null)
+    {
+      throw new RuntimeException("Unable to create FoodItem due to aMacroDistribution. See http://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
+    }
+    macroDistribution = aMacroDistribution;
+  }
+
+  public FoodItem(String aName, int aCalories, float aPortionSize, float aFatsForMacroDistribution, float aCarbsForMacroDistribution, float aProteinForMacroDistribution, Goal aGoalForMacroDistribution)
+  {
+    name = aName;
+    calories = aCalories;
+    portionSize = aPortionSize;
+    macroDistribution = new MacroDistribution(aFatsForMacroDistribution, aCarbsForMacroDistribution, aProteinForMacroDistribution);
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public boolean setName(String aName)
+  {
+    boolean wasSet = false;
+    name = aName;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setCalories(int aCalories)
+  {
+    boolean wasSet = false;
+    calories = aCalories;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setPortionSize(float aPortionSize)
+  {
+    boolean wasSet = false;
+    portionSize = aPortionSize;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public int getCalories()
+  {
+    return calories;
+  }
+
+  public float getPortionSize()
+  {
+    return portionSize;
+  }
+  /* Code from template association_GetOne */
+  public MacroDistribution getMacroDistribution()
+  {
+    return macroDistribution;
+  }
+  /* Code from template association_SetOneToMandatoryMany */
+
+  public void delete()
+  {
+    MacroDistribution existingMacroDistribution = macroDistribution;
+    macroDistribution = null;
+  }
+
+  public String toString()
+  {
+    return super.toString() + "["+
+            "name" + ":" + getName()+ "," +
+            "calories" + ":" + getCalories()+ "," +
+            "portionSize" + ":" + getPortionSize()+ "]" + System.getProperties().getProperty("line.separator") +
+            "  " + "macroDistribution = "+(getMacroDistribution()!=null?Integer.toHexString(System.identityHashCode(getMacroDistribution())):"null");
+  }
+}
---
+++ Server/fitboi/src/main/java/com/ecse428/project/fitboi/model/Metrics.java
@@ -0,0 +1,174 @@
+package com.ecse428.project.fitboi.model;
+
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.29.1.4753.5a97eca04 modeling language!*/
+
+
+import java.sql.Date;
+import java.util.*;
+
+// line 20 "model.ump"
+// line 80 "model.ump"
+public class Metrics
+{
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //Metrics Attributes
+  private Date date;
+  private int exerciseSpending;
+
+  //Metrics Associations
+  private List<Meal> meals;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public Metrics(Date aDate, int aExerciseSpending)
+  {
+    date = aDate;
+    exerciseSpending = aExerciseSpending;
+    meals = new ArrayList<Meal>();
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public boolean setDate(Date aDate)
+  {
+    boolean wasSet = false;
+    date = aDate;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setExerciseSpending(int aExerciseSpending)
+  {
+    boolean wasSet = false;
+    exerciseSpending = aExerciseSpending;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public Date getDate()
+  {
+    return date;
+  }
+
+  public int getExerciseSpending()
+  {
+    return exerciseSpending;
+  }
+  /* Code from template association_GetMany */
+  public Meal getMeal(int index)
+  {
+    Meal aMeal = meals.get(index);
+    return aMeal;
+  }
+
+  public List<Meal> getMeals()
+  {
+    List<Meal> newMeals = Collections.unmodifiableList(meals);
+    return newMeals;
+  }
+
+  public int numberOfMeals()
+  {
+    int number = meals.size();
+    return number;
+  }
+
+  public boolean hasMeals()
+  {
+    boolean has = meals.size() > 0;
+    return has;
+  }
+
+  public int indexOfMeal(Meal aMeal)
+  {
+    int index = meals.indexOf(aMeal);
+    return index;
+  }
+
+  /* Code from template association_MinimumNumberOfMethod */
+  public static int minimumNumberOfMeals()
+  {
+    return 0;
+  }
+
+  public boolean addMeal(Meal aMeal)
+  {
+    boolean wasAdded = false;
+    if (meals.contains(aMeal)) {
+      return false;
+    }
+    meals.add(aMeal);
+    wasAdded = true;
+    return wasAdded;
+  }
+
+  public boolean removeMeal(Meal aMeal)
+  {
+   if(!meals.contains(aMeal)){
+     return false;
+   }
+   else {
+     meals.remove(aMeal);
+     return true;
+   }
+  }
+  /* Code from template association_AddIndexControlFunctions */
+  public boolean addMealAt(Meal aMeal, int index)
+  {  
+    boolean wasAdded = false;
+    if(addMeal(aMeal))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfMeals()) { index = numberOfMeals() - 1; }
+      meals.remove(aMeal);
+      meals.add(index, aMeal);
+      wasAdded = true;
+    }
+    return wasAdded;
+  }
+
+  public boolean addOrMoveMealAt(Meal aMeal, int index)
+  {
+    boolean wasAdded = false;
+    if(meals.contains(aMeal))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfMeals()) { index = numberOfMeals() - 1; }
+      meals.remove(aMeal);
+      meals.add(index, aMeal);
+      wasAdded = true;
+    } 
+    else 
+    {
+      wasAdded = addMealAt(aMeal, index);
+    }
+    return wasAdded;
+  }
+
+  public void delete()
+  {
+    for(int i=meals.size(); i > 0; i--)
+    {
+      Meal aMeal = meals.get(i - 1);
+      aMeal.delete();
+    }
+  }
+
+
+  public String toString()
+  {
+    return super.toString() + "["+
+            "exerciseSpending" + ":" + getExerciseSpending()+ "]" + System.getProperties().getProperty("line.separator") +
+            "  " + "date" + "=" +
+            (getDate() != null ? !getDate().equals(this)  ? getDate().toString().replaceAll("  ","    ") : "this" : "null") +
+            System.getProperties().getProperty("line.separator");
+  }
+}
