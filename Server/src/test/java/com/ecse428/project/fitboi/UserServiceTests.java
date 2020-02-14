ca3b666604f195ee802c0cb16d760a291f901fd2
msukkari <michael.sukkarieh@mail.mcgill.ca>
2020-02-14 11:35:36 -0500

    added user service skeleton

---
+++ Server/src/test/java/com/ecse428/project/fitboi/UserServiceTests.java
@@ -0,0 +1,15 @@
+package com.ecse428.project.fitboi;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@SpringBootTest
+class UserServiceTests {
+
+}
