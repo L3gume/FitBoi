578cd9a2f7ad8621f041e1b149a96f50c7214033
msukkari <michael.sukkarieh@mail.mcgill.ca>
2020-02-14 12:13:23 -0500

    wrote user service methods for get user goals

--- Server/src/main/java/com/ecse428/project/fitboi/service/UserService.java
+++ Server/src/main/java/com/ecse428/project/fitboi/service/UserService.java
@@ -1,6 +1,13 @@
 package com.ecse428.project.fitboi.service;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.ecse428.project.fitboi.model.Goal;
 import com.ecse428.project.fitboi.model.UserProfile;
 import com.ecse428.project.repository.UserRepository;
 
@@ -64,5 +71,27 @@ public class UserService {
 		return deletedUser;
 	}
 	
+	public List<Goal> getUserGoals(String userEmail)
+	{
+		UserProfile user = getUser(userEmail);
+		return user.getGoals();
+	}
 
+	public List<Goal> getUserGoalsInRange(String userEmail, Date start,
+		Date end)
+	{
+		List<Goal> goals = getUserGoals(userEmail);
+		List<Goal> valid_goals = new ArrayList<Goal>();
+
+		for(Goal goal : goals)
+		{
+			if((goal.getStartDate().after(start) && goal.getStartDate().before(end)) ||
+				goal.getStartDate().equals(start) || goal.getStartDate().equals(end))
+			{
+				valid_goals.add(goal);
+			}
+		}
+
+		return valid_goals;
+	}
 }
