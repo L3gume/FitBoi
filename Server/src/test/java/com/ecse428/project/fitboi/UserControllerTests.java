1f0f99fdd6e7b455fee87d484c5fe58b7fca8a9a
He Qian Wang <heqianw@gmail.com>
2020-02-14 17:54:03 -0500

    Added jackson dependency and created first controller test that we can use

---
+++ Server/src/test/java/com/ecse428/project/fitboi/UserControllerTests.java
@@ -0,0 +1,69 @@
+package com.ecse428.project.fitboi;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import org.springframework.http.MediaType;
+
+import com.ecse428.project.fitboi.model.UserProfile;
+import com.ecse428.project.repository.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.logging.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTests {
+	private static final Logger LOGGER = Logger.getLogger( UserControllerTests.class.getName() );
+
+	@Autowired 
+	private UserRepository userRepository;
+
+	@Autowired
+    private MockMvc mockMvc;
+
+	private	ObjectMapper mapper = new ObjectMapper();
+
+	@BeforeEach
+	public void setup(){
+		userRepository.deleteAll();
+	}
+
+	@AfterEach
+	public void teardown(){
+		userRepository.deleteAll();
+	}
+
+	@Test
+	public void testPostUserController() throws Exception{
+		String aEmail = "testUser1@mail.mcgill.ca";
+		String aName = "testboi";
+		String aUserName = "testBoi";
+		String aPassword = "password";
+		int aAge = 15;
+		int aHeight = 193;
+		boolean aBiologicalSex = true;
+		UserProfile testUser = new UserProfile(aEmail, aName, aUserName, aPassword, aAge, aHeight, aBiologicalSex);
+
+		LOGGER.info(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(testUser));
+		mockMvc.perform(post("/users/").contentType(MediaType.APPLICATION_JSON)
+        .content(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(testUser)))
+        .andExpect(status().isCreated());
+	}
+
+}
