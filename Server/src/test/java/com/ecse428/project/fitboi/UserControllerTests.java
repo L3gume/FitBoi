b1d5afb840043e65e0de2cb1ccf6a388e4f3b04c
EricZimmermann <eric.zimmermann@mail.mcgill.ca>
2020-02-14 22:01:51 -0500

    working status tests added

--- Server/src/test/java/com/ecse428/project/fitboi/UserControllerTests.java
+++ Server/src/test/java/com/ecse428/project/fitboi/UserControllerTests.java
@@ -10,7 +10,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.http.MediaType;
 
@@ -44,7 +45,7 @@ class UserControllerTests {
 	}
 
 	@Test
-	public void testPostUserController() throws Exception{
+	public void testPostUserControllerSuccess() throws Exception{
 		String aEmail = "testUser1@mail.mcgill.ca";
 		String aName = "testboi";
 		String aUserName = "testBoi";
@@ -58,6 +59,76 @@ class UserControllerTests {
 		mockMvc.perform(post("/users/").contentType(MediaType.APPLICATION_JSON)
         	.content(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(testUser)))
         	.andExpect(status().isCreated());
+	}
+
+	@Test
+	public void testPostUserControllerNull() throws Exception{
+		UserProfile testUser = null;
+
+		LOGGER.info(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(testUser));
+		mockMvc.perform(post("/users/").contentType(MediaType.APPLICATION_JSON)
+        	.content(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(testUser)))
+        	.andExpect(status().isBadRequest());
+	}
+
+	@Test
+	public void testPutUserControllerSuccess() throws Exception{
+		String aEmail = "testUser1@mail.mcgill.ca";
+		String aName = "testboi";
+		String aUserName = "testBoi";
+		String aPassword = "password";
+		int aAge = 15;
+		int aHeight = 193;
+		boolean aBiologicalSex = true;
+		UserProfile testUser = new UserProfile(aEmail, aName, aUserName, aPassword, aAge, aHeight, aBiologicalSex);
+
+		LOGGER.info(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(testUser));
+		mockMvc.perform(post("/users/").contentType(MediaType.APPLICATION_JSON)
+        	.content(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(testUser)))
+        	.andExpect(status().isCreated());
+
+        testUser.setName("updatedBoi");
+        mockMvc.perform(put("/users/").contentType(MediaType.APPLICATION_JSON)
+        	.content(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(testUser)))
+        	.andExpect(status().isOk());
+	}
+
+	@Test
+	public void testGetUserControllerSuccess() throws Exception{
+		String aEmail = "testUser1@mail.mcgill.ca";
+		String aName = "testboi";
+		String aUserName = "testBoi";
+		String aPassword = "password";
+		int aAge = 15;
+		int aHeight = 193;
+		boolean aBiologicalSex = true;
+		UserProfile testUser = new UserProfile(aEmail, aName, aUserName, aPassword, aAge, aHeight, aBiologicalSex);
+
+		LOGGER.info(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(testUser));
+		mockMvc.perform(post("/users/").contentType(MediaType.APPLICATION_JSON)
+        	.content(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(testUser)))
+        	.andExpect(status().isCreated());
+
+        mockMvc.perform(get("/users/"+aEmail)).andExpect(status().isOk());
+	}
+
+	@Test
+	public void testDeleteUserControllerSuccess() throws Exception{
+		String aEmail = "testUser1@mail.mcgill.ca";
+		String aName = "testboi";
+		String aUserName = "testBoi";
+		String aPassword = "password";
+		int aAge = 15;
+		int aHeight = 193;
+		boolean aBiologicalSex = true;
+		UserProfile testUser = new UserProfile(aEmail, aName, aUserName, aPassword, aAge, aHeight, aBiologicalSex);
+
+		LOGGER.info(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(testUser));
+		mockMvc.perform(post("/users/").contentType(MediaType.APPLICATION_JSON)
+        	.content(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(testUser)))
+        	.andExpect(status().isCreated());
+
+        mockMvc.perform(delete("/users/"+aEmail)).andExpect(status().isOk());
 	}
 
 }
