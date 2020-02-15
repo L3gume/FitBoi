1d33eee8b4de73ac4914779b648d1d48469c254f
Julien Lesaffre <julien.lesaffre@mail.mcgill.ca>
2020-02-15 13:46:37 -0500

    S7-03: Create RESTful controller for user's goals (#80)
    * GoalDto file added
    
    * GoalDto created
    
    * wrote user service methods for get user goals
    
    * GoalService and GoalRepository created
    
    * merged s16 changes into create goal changes
    
    * working on issue 36
    
    * added controller method for getting all goals
    
    * ready to merge
    
    * create a goal for a user works
    
    * RESTful controller for user's goals finished
    
    Co-authored-by: Michael Sukkarieh <michael.sukkarieh@mail.mcgill.ca>

--- Server/src/main/java/com/ecse428/project/fitboi/controller/UserController.java
+++ Server/src/main/java/com/ecse428/project/fitboi/controller/UserController.java
@@ -2,6 +2,8 @@ package com.ecse428.project.fitboi.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.Date;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,15 +18,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecse428.project.fitboi.dto.*;
+import com.ecse428.project.fitboi.model.ActivityLevel;
+import com.ecse428.project.fitboi.model.Goal;
+import com.ecse428.project.fitboi.model.MacroDistribution;
 import com.ecse428.project.fitboi.model.UserProfile;
+import com.ecse428.project.fitboi.service.GoalService;
 import com.ecse428.project.fitboi.service.UserService;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @RestController
 @RequestMapping("/users/")
 public class UserController {
 	
 	@Autowired
-	private UserService userService;
+    private UserService userService;
+    
+    @Autowired
+    private GoalService goalService;
 	
     /**
      * GET
@@ -68,7 +78,7 @@ public class UserController {
     	
     	System.out.println("USER: " + user.toString());
     	
-    	if (!userService.addUser(convertToDomainObject(user)))
+    	if (!userService.addNewUser(convertToDomainObject(user)))
     	{
     		return new ResponseEntity<String>("User already exists", HttpStatus.BAD_REQUEST);
     	}
@@ -117,6 +127,81 @@ public class UserController {
     	return new ResponseEntity<UserDto>(convertToDto(deletedUser), HttpStatus.OK);
     }
 
+    /**
+     * GET
+     * /users/{user_id}/ -> returns a specific user given their userId (email for now)
+     * @param userId
+     * @return
+     */
+    @GetMapping("{userEmail}/goals")
+    public ResponseEntity<List<GoalDto>> getUserGoals(@PathVariable String userEmail)
+    {
+        List<GoalDto> goalDtos = new ArrayList<GoalDto>();
+        List<Goal> goals = goalService.getUserGoals(userEmail);
+
+        for(Goal goal : goals)
+        {
+            goalDtos.add(new GoalDto(
+                goal.getBaseCalories(),
+                goal.isResult(),
+                goal.getStartDate(),
+                goal.getWeight(),
+                goal.getActivityLevel(),
+                goal.getMacroDistribution()
+            ));
+        }
+
+    	return new ResponseEntity<List<GoalDto>>(goalDtos, HttpStatus.OK);
+    }
+
+        /**
+     * POST
+     * create a new Goal for a User
+     * @param userId
+     * @return
+     */
+    @PostMapping("{userId}/goals")
+    public ResponseEntity<?> createGoal(@PathVariable("userId") String userEmail, @RequestBody ObjectNode objectNode) {
+    	if (objectNode == null) {
+    		return new ResponseEntity<String>("Request body invalid", HttpStatus.NOT_ACCEPTABLE);
+    	}
+        UserProfile user = userService.getUser(userEmail);
+
+        ActivityLevel activityLevel = ActivityLevel.valueOf(objectNode.get("activityLevel").asText());
+
+        MacroDistribution macroDistribution = new MacroDistribution((float) objectNode.get("fats").asLong(), (float) objectNode.get("carbs").asLong(), (float) objectNode.get("proteins").asLong());
+
+        GoalDto goal = new GoalDto(objectNode.get("baseCalories").asInt(), objectNode.get("result").asBoolean(), Date.valueOf(objectNode.get("startDate").asText()), (float) objectNode.get("weight").asLong(), 
+        activityLevel, macroDistribution);
+
+    	if (!goalService.addGoaltoUser(convertToDomainObject(goal), user))
+    	{
+    		return new ResponseEntity<String>("Goal already exists", HttpStatus.UNPROCESSABLE_ENTITY);
+    	}
+
+    	return new ResponseEntity<GoalDto>(goal, HttpStatus.CREATED);
+    }
+
+        /**
+     * DELETE
+     * /users/{user_id}/goals/{goalId} -> deletes a goal from the DB
+     * @param userId 
+     * @param goalId
+     * @return
+     */
+    @DeleteMapping("{userId}/goals/{goalId}")
+    public ResponseEntity<?> deleteGoal(@PathVariable String userId, @PathVariable int goalId) {
+    	
+    	Goal deletedGoal = goalService.deleteGoal(userId, goalId);
+
+    	if (deletedGoal == null) {
+    		return new ResponseEntity<String>("Goal does not exist", HttpStatus.NOT_FOUND);
+    	}
+    	return new ResponseEntity<GoalDto>(convertToDto(deletedGoal), HttpStatus.OK);
+    }
+
+
+
    // TODO: Add PATCH call
     
     private UserDto convertToDto(UserProfile user) {
@@ -128,6 +213,17 @@ public class UserController {
     			user.getAge(),
     			user.getHeight(),
     			user.getBiologicalSex()
+    			);
+    }
+
+    private GoalDto convertToDto(Goal goal) {
+    	return new GoalDto(
+            goal.getBaseCalories(),
+            goal.getResult(),
+            goal.getStartDate(),
+            goal.getWeight(),
+            goal.getActivityLevel(),
+            goal.getMacroDistribution()   			
     			);
     }
     
@@ -143,6 +239,20 @@ public class UserController {
 				userDto.getSex()
 				);
 		return profile;
+    }
+
+    private Goal convertToDomainObject(GoalDto goalDto) {
+		
+		Goal goal = new Goal(
+            goalDto.getBaseCalories(),
+            goalDto.getResult(),
+            goalDto.getStartDate(),
+            goalDto.getWeight(),
+            goalDto.getActivityLevel(),
+            goalDto.getMacroDistribution()
+                );
+                
+		return goal;
     }
 
 }
