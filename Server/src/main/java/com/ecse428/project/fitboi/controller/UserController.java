e9280bf2f4285b9447ac8026497d8664d41bd252
He Qian Wang <heqianw@gmail.com>
2020-02-16 00:05:19 -0500

    Feature/s3 validate info (#85)
    * Initial Commit with functional Spring Boot Web Server
    
    * Added tests to validate info for gherkins
    
    * removed extra imports
    
    * Fixed typo: changed && to ||

--- Server/src/main/java/com/ecse428/project/fitboi/controller/UserController.java
+++ Server/src/main/java/com/ecse428/project/fitboi/controller/UserController.java
@@ -58,7 +58,7 @@ public class UserController {
     @GetMapping("{userEmail}")
     public ResponseEntity<?> getUser(@PathVariable String userEmail) {
     	UserProfile user = userService.getUser(userEmail);
-    	if (user == null) {
+    	if (user == null || !userEmail.contains("@")) {
     		return new ResponseEntity<String>("User not found", HttpStatus.NOT_FOUND);
     	}
     	return new ResponseEntity<UserDto>(convertToDto(user), HttpStatus.OK);
@@ -72,7 +72,7 @@ public class UserController {
      */
     @PostMapping("")
     public ResponseEntity<?> addUser(@RequestBody UserDto user) {
-    	if (user == null) {
+    	if (user == null || !isValidUser(user)) {
     		return new ResponseEntity<String>("Request body invalid", HttpStatus.NOT_ACCEPTABLE);
     	}
     	
@@ -95,7 +95,7 @@ public class UserController {
      */
     @PutMapping("")
     public ResponseEntity<?> updateUser(@RequestBody UserDto user) {
-        if (user == null) {
+        if (user == null || !isValidUser(user)) {
             return new ResponseEntity<String>("Request body invalid", HttpStatus.NOT_ACCEPTABLE);
         }
         
@@ -121,7 +121,7 @@ public class UserController {
     	
     	UserProfile deletedUser = userService.deleteUser(userEmail);
 
-    	if (deletedUser == null) {
+    	if (deletedUser == null || !userEmail.contains("@")) {
     		return new ResponseEntity<String>("User does not exist", HttpStatus.NOT_FOUND);
     	}
     	return new ResponseEntity<UserDto>(convertToDto(deletedUser), HttpStatus.OK);
@@ -253,6 +253,10 @@ public class UserController {
                 );
                 
 		return goal;
+    }
+
+    private boolean isValidUser(UserDto user){
+        return user.getEmail().contains(("@")) && user.getAge() > 0 && user.getHeight() > 0;
     }
 
 }
