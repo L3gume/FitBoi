4c717c288979dba9de29a4ac598896d4c45d9c1e
JulienLesaffre <julien.lesaffre@mail.mcgill.ca>
2020-02-17 17:24:43 -0500

    gherkin script for searching food item started

--- App/app/src/androidTest/java/com/example/fitboi/cucumber/steps/ID003_User_Logs_A_Food_Item.java
+++ App/app/src/androidTest/java/com/example/fitboi/cucumber/steps/ID003_User_Logs_A_Food_Item.java
@@ -1,4 +1,60 @@
 package com.example.fitboi.cucumber.steps;
 
+import com.ecse428.project.fitboi.model.DBFood;
+import com.example.fitboi.data.model.LoggedInUser;
+import com.example.fitboi.dto.UserDto;
+import com.example.fitboi.api.FoodItemAPI;
+import java.util.function.Consumer;
+
+
+import cucumber.api.java.en.*;
+
+import static org.mockito.Mockito.mock;
+
 public class ID003_User_Logs_A_Food_Item {
+    private UserDto userDto = null;
+    private LoggedInUser user = null;
+    List<DBFood> database = new ArrayList<DBFood>();
+    String foodLoggedName = "";
+
+    public List<DBFood> isFoodInDatabase(String name)
+    {
+        for(DBFood food : database)
+        {
+            if(food.getName().equals(name)) return true;
+        }
+
+        return false;
+    }
+
+    @Given("the User is not currently logged into the FitBoi application")
+    public void given_user_not_currently_logged_into_fitboi_app() {
+        assert user == null;
+    }
+
+    
+    @And("the database contains the desired food item")
+    public void and_database_contains_desired_food_item() {
+        DBFood foodItem = new DBFood("Bread", 100, 120, 100, 25); 
+        database.add(foodItem);
+        foodLoggedName = "Bread";
+        assert isFoodInDatabase(foodLoggedName);
+    }
+
+    @And("the database does not contains the desired food item")
+    public void and_database_contains_desired_food_item() {
+        DBFood foodItem = new DBFood("Cheese", 120, 10, 300, 25); 
+        database.add(foodItem);
+        foodLoggedName = "Bread";
+        assert !isFoodInDatabase(foodLoggedName);
+    }
+
+    /*
+    @Then("the User's calorie count is updated using the desired food item")
+    public void then_user_calorie_count_updated_using_desired_food_item() {
+        assert true;
+    }
+*/
+
+
 }
