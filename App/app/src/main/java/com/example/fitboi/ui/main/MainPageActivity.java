b8e26aa01404d0a721c1f8b9941c745d7a9e0837
Andi Bakti <andi-camille.bakti@mail.mcgill.ca>
2020-02-17 15:59:55 -0500

    Gradlew script fix & Main page polish (#104)
    * Started creating main scheme for Main UI Page
    
    * Create UI layout for home page
    
    * Created .xml layout file with placeholder images/logos
    * Added placeholder call in login to create home page for testing
    * added strings for testing
    
    * Fix naming string for consistency
    
    * Add 'add meal' UI page
    
    Add array adapter for list view
    Create placeholder for Meal (maybe backend already has this handled?)
    Import fitboi logo
    
    * Change search button to searchView
    
    Preparing to work on issue #32
    
    * Convert ListView to RecyclerView for searchfood
    
    * Prepare for merge w/ Ryan
    's branch
    
    * Fix method call
    
    * Prepare for merge with dev
    
    * Add main page drawables
    
    * .gitignore fix
    
    * Remove trailing 'src' folder
    
    * Improve gitignore
    
    * .gitignore fix
    
    * try .gitignore fix again
    
    * try again
    
    * Fix .gitignore typo
    
    * actually remove that file
    
    * Fix merge issue
    
    * Allow for create meal activity
    
    * Finished general Layout for Main Page, having gradle build errors when running Activities on Emulator
    
    * Update gradlew script
    
    * Fix syntax issues in xml
    
    * Add empty view for meal creation
    
    * Add cucumber report to gitignore
    
    Co-authored-by: Michel Abdel Nour <michel.abdelnour16@gmail.com>

--- App/app/src/main/java/com/example/fitboi/ui/main/MainPageActivity.java
+++ App/app/src/main/java/com/example/fitboi/ui/main/MainPageActivity.java
@@ -11,12 +11,7 @@ import com.example.fitboi.ui.meal.AddMealActivity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
-/**
- * Created by Andi-Camille Bakti on 13/02/2020.
- */
 public class MainPageActivity extends AppCompatActivity {
-    private static final String LOG_TAG = MainPageActivity.class.getSimpleName();
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,6 +23,7 @@ public class MainPageActivity extends AppCompatActivity {
 
     private void uiSetup() {
         getSupportActionBar().setTitle("Home");
+
         final CardView createMeal = findViewById(R.id.main_meals_card);
 
         createMeal.setOnClickListener(new View.OnClickListener() {
@@ -36,11 +32,31 @@ public class MainPageActivity extends AppCompatActivity {
                 launchCreateMealActivity();
             }
         });
+        final ImageButton profileButton = findViewById((R.id.profile_button));
+
+        createMeal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchCreateMealActivity();
+            }
+        });
+
+        profileButton.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchUserProfileActivity();
+            }
+        }));
 
     }
 
     private void launchCreateMealActivity() {
         Intent intent = new Intent(this, AddMealActivity.class);
+        startActivity(intent);
+    }
+
+    private void launchUserProfileActivity() {
+        Intent intent = new Intent(this, UserProfileActivity.class);
         startActivity(intent);
     }
 }
