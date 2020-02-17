232387c7fde4936d8c112a7f2995f574e7e7c8ef
eliottbourachot <eliott.bourachot@gmail.com>
2020-02-17 17:42:41 -0500

    S7-07: gherkin script for adding new goal

--- App/app/src/androidTest/java/com/example/fitboi/cucumber/assets/features/ID006_User_Sets_A_New_Goal.feature
+++ App/app/src/androidTest/java/com/example/fitboi/cucumber/assets/features/ID006_User_Sets_A_New_Goal.feature
@@ -9,47 +9,40 @@ Scenario: User successfully adds a preset goal with no current goal in progress 
 Given the User is on the goals dashboard
 And there is no goal currently in progress
 And the User has selected one of three preset goals
-When clicking the set goal button
 Then the User adds a goal to their profile
-And the "Successfully added new goal" message is issued
+And the 'Successfully added new goal' message is issued
 
 Scenario: User successfully adds a preset goal with a current goal in progress (Normal flow)
 
 Given the User is on the goals dashboard
 And there is a goal currently in progress
 And the User has selected one of three preset goals
-When clicking the set goal button
 Then the User is given a prompt informing that they will override their current goal
-And the User clicks the confirm button
 Then the User adds a goal to their profile
-And the "Successfully added new goal" message is issued
+And the 'Successfully added new goal' message is issued
 
 Scenario: User successfully adds a custom goal with no current goal in progress (Alternate flow)
 
 Given the User is on the goals dashboard
 And there is no goal currently in progress
 And the User has entered a custom goal
-When clicking the set goal button
 Then the User adds a goal to their profile
-And the "Successfully added new custom goal" message is issued
+And the 'Successfully added new custom goal' message is issued
 
 Scenario: User successfully adds a custom goal with a current goal in progress (Alternate flow)
 
 Given the User is on the goals dashboard
 And there is a goal currently in progress
 And the User has entered a custom goal
-When clicking the set goal button
 Then the User is given a prompt informing that they will override their current goal
-And the User clicks the confirm button
 Then the User adds a goal to their profile
-And the "Successfully added new custom goal" message is issued
+And the 'Successfully added new custom goal' message is issued
 
 Scenario: User enters invalid data (Error flow)
 
 Given the User is on the goals dashboard
 And the User has entered a custom goal
 And the data is invalid
-When clicking the set goal button
-Then a User adds a goal to their profile
+Then the User adds a goal to their profile
 And the User is given a prompt informing that they will override their current goal
-And the "Input invalid" message is issued
+And the 'Input invalid' message is issued
--- App/app/src/androidTest/java/com/example/fitboi/cucumber/steps/ID006_User_Sets_A_New_Goal.java
+++ App/app/src/androidTest/java/com/example/fitboi/cucumber/steps/ID006_User_Sets_A_New_Goal.java
@@ -1,4 +1,85 @@
 package com.example.fitboi.cucumber.steps;
 
+import com.example.fitboi.api.GoalAPI;
+import com.example.fitboi.dto.GoalDto;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.And;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+import java.util.function.Consumer;
+
+@RunWith(Cucumber.class)
 public class ID006_User_Sets_A_New_Goal {
+
+    @Given("the User is on the goals dashboard")
+    public void the_user_is_on_the_goals_dashboard() {
+        assert true;
+    }
+
+    @Then("the User adds a goal to their profile")
+    public void the_user_adds_a_goal_to_their_profile() {
+        GoalDto goalDto = new GoalDto(10, true, "12-12-2019", 12, "High", 20, 30, 40);
+        Consumer addGoalConfirmation = new Consumer<GoalDto>() {
+            @Override
+            public void accept(GoalDto goal) {
+            }
+        };
+        GoalAPI.addNewGoal("test@gmail.com", goalDto, addGoalConfirmation);
+        assert true;
+    }
+
+    @And("there is no goal currently in progress")
+    public void there_is_no_goal_currently_in_progress() {
+        assert true;
+    }
+
+    @And("the User has selected one of three preset goals")
+    public void the_user_has_selected_one_of_three_preset_goals() {
+        assert true;
+    }
+
+    @And("the 'Successfully added new goal' message is issued")
+    public void the_successfully_added_new_goal_message_is_issued() {
+        assert true;
+    }
+
+    @And("there is a goal currently in progress")
+    public void there_is_a_goal_currently_in_progress() {
+        Consumer getGoal = new Consumer<GoalDto>() {
+            @Override
+            public void accept(GoalDto goal) {
+            }
+        };
+        GoalAPI.getUserGoals("test@gmail.com",getGoal);
+        assert true;
+    }
+
+    @And("the User has entered a custom goal")
+    public void the_user_has_entered_a_custom_goal() {
+        assert true;
+    }
+
+    @And("the 'Successfully added new custom goal' message is issued")
+    public void the_successfully_added_new_custom_goal_message_is_issued() {
+        assert true;
+    }
+
+    @And("the data is invalid")
+    public void the_data_is_invalid() {
+        assert true;
+    }
+
+    @And("the User is given a prompt informing that they will override their current goal")
+    public void the_user_is_given_a_prompt_informing_that_they_will_override_their_current_goal() {
+        assert true;
+    }
+
+    @And("the 'Input invalid' message is issued")
+    public void the_input_invalid_message_is_issued() {
+        assert true;
+    }
+
 }
