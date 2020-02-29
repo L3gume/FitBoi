a6f14cb25020bcc58699824e13cc95f67593ceb1
eliottbourachot <eliott.bourachot@gmail.com>
2020-02-29 18:30:50 -0500

    Modified DTOs to match new domain model

--- Server/src/main/java/com/ecse428/project/fitboi/dto/DBFoodDto.java
+++ Server/src/main/java/com/ecse428/project/fitboi/dto/DBFoodDto.java
@@ -2,81 +2,100 @@ package com.ecse428.project.fitboi.dto;
 
 public class DBFoodDto
 {
+    private int id;
     private String name;
     private long cal;
     private double protein;
     private double fat;
     private double carbs;
 
-
-    public DBFoodDto(String aName, long aCal, double aProtein, double aFat,
-        double aCarbs)
-    {
-        name = aName;
-        cal = aCal;
-        protein = aProtein;
-        fat = aFat;
-        carbs = aCarbs;
-    }
-
-    public DBFoodDto()
-    {
+    public DBFoodDto() {
 
     }
 
-    public boolean setName(String aName)
-    {
-        this.name = aName;
-        return true;
+    public DBFoodDto(int id, String name, long cal, double protein, double fat, double carbs) {
+        this.id = id;
+        this.name = name;
+        this.cal = cal;
+        this.protein = protein;
+        this.fat = fat;
+        this.carbs = carbs;
     }
 
-    public boolean setCal(long aCal)
-    {
-        this.cal = aCal;
-        return true;
+    public int getId() {
+        return id;
     }
 
-    public boolean setProtein(double aProtein)
-    {
-        this.protein = aProtein;
-        return true;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public boolean setFat(double aFat)
-    {
-        this.fat = aFat;
-        return true;
+    public String getName() {
+        return name;
     }
 
-    public boolean setCarbs(double aCarbs)
-    {
-        this.carbs = aCarbs;
-        return true;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getName()
-    {
-        return this.name;
+    public long getCal() {
+        return cal;
     }
 
-    public long getCal()
-    {
-        return this.cal;
+    public void setCal(long cal) {
+        this.cal = cal;
     }
 
-    public double getProtein()
-    {
-        return this.protein;
+    public double getProtein() {
+        return protein;
     }
 
-    public double getFat()
-    {
-        return this.fat;
+    public void setProtein(double protein) {
+        this.protein = protein;
     }
 
-    public double getCarbs()
-    {
-        return this.carbs;
+    public double getFat() {
+        return fat;
     }
 
+    public void setFat(double fat) {
+        this.fat = fat;
+    }
+
+    public double getCarbs() {
+        return carbs;
+    }
+
+    public void setCarbs(double carbs) {
+        this.carbs = carbs;
+    }
+
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+        DBFoodDto dbFoodDto = (DBFoodDto) object;
+        return id == dbFoodDto.id &&
+                cal == dbFoodDto.cal &&
+                java.lang.Double.compare(dbFoodDto.protein, protein) == 0 &&
+                java.lang.Double.compare(dbFoodDto.fat, fat) == 0 &&
+                java.lang.Double.compare(dbFoodDto.carbs, carbs) == 0 &&
+                name.equals(dbFoodDto.name);
+    }
+
+    public int hashCode() {
+        return java.util.Objects.hash(super.hashCode(), id, name, cal, protein, fat, carbs);
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "DBFoodDto{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", cal=" + cal +
+                ", protein=" + protein +
+                ", fat=" + fat +
+                ", carbs=" + carbs +
+                '}';
+    }
 }
---
+++ Server/src/main/java/com/ecse428/project/fitboi/dto/MetricDto.java
@@ -0,0 +1,63 @@
+
+public class MetricDto {
+
+    private int id;
+    private String date;
+    private int exerciseSpending;
+
+    public MetricDto() {
+    }
+
+    public MetricDto(int id, String date, int exerciseSpending) {
+        this.id = id;
+        this.date = date;
+        this.exerciseSpending = exerciseSpending;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getExerciseSpending() {
+        return exerciseSpending;
+    }
+
+    public void setExerciseSpending(int exerciseSpending) {
+        this.exerciseSpending = exerciseSpending;
+    }
+
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+        MetricDto metricDto = (MetricDto) object;
+        return id == metricDto.id &&
+                exerciseSpending == metricDto.exerciseSpending &&
+                date.equals(metricDto.date);
+    }
+
+    public int hashCode() {
+        return java.util.Objects.hash(super.hashCode(), id, date, exerciseSpending);
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "MetricDto{" +
+                "id=" + id +
+                ", date='" + date + '\'' +
+                ", exerciseSpending=" + exerciseSpending +
+                '}';
+    }
+}
