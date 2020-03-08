55f6599f87cd30c34098610d58dd2e77c23a3347
L3gume <justin.tremblay@mail.mcgill.ca>
2020-03-08 18:53:50 -0400

    Slightly simplified S10 feature file. Implemented S10 acceptance tests

--- App/app/src/androidTest/java/com/example/fitboi/cucumber/assets/features/ID010_User_Changes_Personal_Profile_Details.feature
+++ App/app/src/androidTest/java/com/example/fitboi/cucumber/assets/features/ID010_User_Changes_Personal_Profile_Details.feature
@@ -7,45 +7,27 @@ So that FitBoi can tailor information to me and provide accurate analytics and t
 Scenario: User successfully changes one profile value (Normal flow)
 
 Given the User is logged into FitBoi
-And they have clicked on the profile settings menu
 And the User has modified one value in their profile
 When clicking the save changes button
 Then the profile changes are saved
-And the "Successfully modified one value" message is issued
 
 Scenario: User successfully changes many or all values (Alternate flow)
 
 Given the User is logged into FitBoi
-And they have clicked on the profile settings menu
 And the User has modified multiple values at once in their profile
 When clicking the save changes button
 Then the profile changes are saved
-And the "Successfully modified multiple value" message is issued
 
 Scenario: User enters invalid data (Error flow)
 
 Given the User is logged into FitBoi
-And they have clicked on the profile settings menu
 And the User has modified multiple values in their profile with invalid data
 When clicking the save changes button
 Then the profile changes are not saved
-And the "Input invalid" message is issued
 
 Scenario: User exits the window without saving (Error flow)
 
 Given the User is logged into FitBoi
-And they have clicked on the profile settings menu
-And the User has modified multiple values in their profile
-When the User exits the current window
+And the User has modified multiple values at once in their profile
+When the User does not save the changes
 Then the profile changes are not saved
-And the "Data was not saved" message is issued
-
-Scenario: Connectivity issue prevents data from persisting (Error flow)
-
-Given the User is logged into FitBoi
-And they have clicked on the profile settings menu
-And the User has modified multiple values in their profile
-And there is a lack of connection between the server and client
-When clicking the save changes button
-Then the profile changes are not saved
-And the "Connectivity issues prevented saving" message is issued
--- App/app/src/androidTest/java/com/example/fitboi/cucumber/steps/ID010_User_Changes_Personal_Profile_Details.java
+++ App/app/src/androidTest/java/com/example/fitboi/cucumber/steps/ID010_User_Changes_Personal_Profile_Details.java
@@ -1,4 +1,63 @@
 package com.example.fitboi.cucumber.steps;
 
+import com.example.fitboi.api.UserAPI;
+import com.example.fitboi.dto.UserDto;
+
+import cucumber.api.java.en.*;
+
 public class ID010_User_Changes_Personal_Profile_Details {
+    private UserDto user;
+    private UserDto updatedUser;
+    private UserDto actualUpdatedUser;
+
+    @Given("the User is logged into FitBoi")
+    public void the_user_is_logged_into_Fitboi() {
+        user = UserAPI.getUser("test@gmail.com",null);
+        if (user == null) {
+            user = new UserDto("test@gmail.com","Test", "test",
+                    "12345","1998-01-01","Male",180);
+            user = UserAPI.addUser(user, null);
+        }
+        updatedUser = new UserDto(user.getEmail(), user.getName(), user.getUserName(),
+                user.getPassword(), user.getDob(), user.getBiologicalSex(), user.getHeight());
+    }
+
+    @And("the User has modified one value in their profile")
+    public void the_user_has_modified_one_value_in_their_profile() {
+        updatedUser.setName("TestTest");
+    }
+
+    @And("the User has modified multiple values at once in their profile")
+    public void the_user_has_modified_multiple_values_in_their_profile() {
+        updatedUser.setName("TestTest");
+        updatedUser.setBiologicalSex("Female");
+    }
+
+    @And("the User has modified multiple values in their profile with invalid data")
+    public void the_user_has_modified_multiple_values_in_their_profile_with_invalid_data() {
+        updatedUser.setHeight(-1);
+        updatedUser.setBiologicalSex("Attack Helicopter");
+    }
+
+    @When("clicking the save changes button")
+    public void clicking_the_save_changes_button() {
+        actualUpdatedUser = UserAPI.updateUser(updatedUser, null);
+    }
+
+    @When("the User does not save the changes")
+    public void user_does_not_save_the_changes() {
+        // Nothing happens
+    }
+
+    @Then("the profile changes are saved")
+    public void the_profile_changes_are_saved() {
+        assert actualUpdatedUser == updatedUser;
+        // revert the change
+        UserAPI.updateUser(user, null);
+    }
+
+    @Then("the profile changes are not saved")
+    public void the_profile_changes_are_not_saved() {
+        assert actualUpdatedUser == null;
+    }
 }
