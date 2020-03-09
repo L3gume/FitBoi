4e2d81aecbe7296decfe4c3c344c425c8ccadcb5
eliottbourachot <eliott.bourachot@gmail.com>
2020-03-09 17:29:55 -0400

    modified feature file and added blanks

---
+++ App/app/src/androidTest/java/com/example/fitboi/cucumber/steps/ID014_User_Logs_Burned_Calories.java
@@ -0,0 +1,60 @@
+package com.example.fitboi.cucumber.steps;
+
+import com.example.fitboi.api.MetricsAPI;
+
+import cucumber.api.java.en.*;
+public class ID014_User_Logs_Burned_Calories {
+
+    private Integer caloriesToAdd;
+    private Integer caloriesToSet;
+
+    @Then("the calories are added to the total burnt calorie count for that day")
+    public void the_calories_are_added_to_the_total_burnt_calorie_count_for_that_day(){
+        
+    }
+
+    @Then("the calories are set as the total burnt calorie count for that day")
+    public void the_calories_are_set_as_the_total_burnt_calorie_count_for_that_day(){
+        
+    }
+
+    @Then("the calories are not added to the total burnt calorie count for that day")
+    public void the_calories_are_not_added_to_the_total_burnt_calorie_count_for_that_day(){
+        
+    }
+
+    @Then("the calories are not set as the total burnt calorie count for that day")
+    public void the_calories_are_not_set_as_the_total_burnt_calorie_count_for_that_day(){
+        
+    }
+
+    @And("the User has entered burned calories to add to their day")
+    public void the_user_has_entered_burned_calories_to_add_to_their_day(){
+        
+    }
+
+    @And("the User decides to add the entered calories to their day")
+    public void the_user_decides_to_add_the_entered_calories_to_their_day(){
+        
+    }
+
+    @And("the User has entered burned calories to set as their day total")
+    public void the_user_has_entered_burned_calories_to_set_as_their_day_total(){
+        
+    }
+
+    @And("the User decides to set the entered calories to their day")
+    public void the_user_decides_to_set_the_entered_calories_to_their_day(){
+        
+    }
+
+    @And("the User has entered invalid data in the burned calories to add to their day")
+    public void the_user_has_entered_invalid_data_in_the_burned_calories_to_add_to_their_day(){
+        
+    }
+
+    @And("the User has entered invalid data in the burned calories to set to their day")
+    public void the_user_has_entered_invalid_data_in_the_burned_calories_to_set_to_their_day(){
+        
+    }
+}
