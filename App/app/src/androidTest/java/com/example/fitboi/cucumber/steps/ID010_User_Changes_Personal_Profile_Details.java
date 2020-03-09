161c28782b22eda3b69678659ec48f78a9dfd87e
Justin Tremblay <11064646+L3gume@users.noreply.github.com>
2020-03-08 23:29:39 -0400

    Slightly simplified S10 feature file. Implemented S10 acceptance tests (#148)
    * Slightly simplified S10 feature file. Implemented S10 acceptance tests
    
    * Made changes as requested in review
    
    * Fixed duplicate test issue. Create StaticTestData class and made 'Given the User is logged into FitBoi' step common to multiple tests
    
    * Added some default initialization values to some StaticTestData members

--- App/app/src/androidTest/java/com/example/fitboi/cucumber/steps/ID010_User_Changes_Personal_Profile_Details.java
+++ App/app/src/androidTest/java/com/example/fitboi/cucumber/steps/ID010_User_Changes_Personal_Profile_Details.java
@@ -1,4 +1,46 @@
 package com.example.fitboi.cucumber.steps;
 
+import com.example.fitboi.api.UserAPI;
+import cucumber.api.java.en.*;
+
 public class ID010_User_Changes_Personal_Profile_Details {
+
+    @And("the User has modified one value in their profile")
+    public void the_user_has_modified_one_value_in_their_profile() {
+        StaticTestData.updatedUser.setName("TestTest");
+    }
+
+    @And("the User has modified multiple values at once in their profile")
+    public void the_user_has_modified_multiple_values_in_their_profile() {
+        StaticTestData.updatedUser.setName("TestTest");
+        StaticTestData.updatedUser.setBiologicalSex("Female");
+    }
+
+    @And("the User has modified multiple values in their profile with invalid data")
+    public void the_user_has_modified_multiple_values_in_their_profile_with_invalid_data() {
+        StaticTestData.updatedUser.setHeight(-1);
+        StaticTestData.updatedUser.setBiologicalSex("Attack Helicopter");
+    }
+
+    @When("the User saves the changes")
+    public void clicking_the_save_changes_button() {
+        StaticTestData.actualUpdatedUser = UserAPI.updateUser(StaticTestData.updatedUser, null);
+    }
+
+    @When("the User does not save the changes")
+    public void user_does_not_save_the_changes() {
+        // Nothing happens
+    }
+
+    @Then("the profile changes are saved")
+    public void the_profile_changes_are_saved() {
+        assert StaticTestData.actualUpdatedUser.equals(StaticTestData.updatedUser);
+        // revert the change
+        UserAPI.updateUser(StaticTestData.user, null);
+    }
+
+    @Then("the profile changes are not saved")
+    public void the_profile_changes_are_not_saved() {
+        assert StaticTestData.actualUpdatedUser == null;
+    }
 }
--- App/app/src/androidTest/java/com/example/fitboi/cucumber/steps/ID015_User_Deletes_Logged_Food_Item.java
+++ App/app/src/androidTest/java/com/example/fitboi/cucumber/steps/ID015_User_Deletes_Logged_Food_Item.java
@@ -1,12 +1,8 @@
 package com.example.fitboi.cucumber.steps;
 
 import com.example.fitboi.api.FoodAPI;
-import com.example.fitboi.api.MealAPI;
-import com.example.fitboi.api.MetricsAPI;
 import com.example.fitboi.api.UserAPI;
 import com.example.fitboi.dto.FoodDto;
-import com.example.fitboi.dto.MealDto;
-import com.example.fitboi.dto.MetricDto;
 import com.example.fitboi.dto.UserDto;
 
 import java.util.List;
@@ -15,58 +11,24 @@ import cucumber.api.java.en.*;
 
 public class ID015_User_Deletes_Logged_Food_Item {
 
-    UserDto user;
-    MetricDto metric;
-    MealDto meal;
-    FoodDto food1 = new FoodDto(0,"Food1", 10,1,20,30,40);
-    FoodDto food2 = new FoodDto(1,"Food2", 50,2,60,70,80);
-    boolean isConnectionGood;
-
-    @Given("the User is logged into FitBoi")
-    public void the_user_is_logged_into_fitboi(){
-        // create user if not already
-        user = UserAPI.getUser("test@gmail.com",null);
-        if (user == null) {
-            user = new UserDto("test@gmail.com","Test", "test", "12345","1998-01-01","Male",180);
-            user = UserAPI.addUser(user, null);
-        }
-        // create metric for user if not already
-        List<MetricDto> metrics = MetricsAPI.getAllUserMetrics(user.getEmail(), null);
-        if (metrics == null || metrics.isEmpty()) {
-            metric = new MetricDto(0,"2020-01-01",0);
-            metric = MetricsAPI.addMetric(user.getEmail(),metric,null);
-        } else {
-            metric = metrics.get(0);
-        }
-        // create meal for metric if not already
-        List<MealDto> meals = MealAPI.getUserMeals(user.getEmail(), metric.getId(), null);
-        if (meals == null || meals.isEmpty()) {
-            meal = new MealDto(0,"Lunch");
-            meal = MealAPI.createMeal(user.getEmail(),metric.getId(),meal,null);
-        } else {
-            meal = meals.get(0);
-        }
-        isConnectionGood = true;
-    }
-
-    @Given("the User has more than one logged food items for the meal")
+    @And("the User has more than one logged food items for the meal")
     public void the_user_has_more_than_one_logged_food_items_for_the_meal(){
-        FoodAPI.addFoodToMeal(food1,user.getEmail(),metric.getId(),meal.getId(),null);
-        FoodAPI.addFoodToMeal(food2,user.getEmail(),metric.getId(),meal.getId(),null);
+        FoodAPI.addFoodToMeal(StaticTestData.food1, StaticTestData.user.getEmail(), StaticTestData.metric.getId(), StaticTestData.meal.getId(),null);
+        FoodAPI.addFoodToMeal(StaticTestData.food2, StaticTestData.user.getEmail(), StaticTestData.metric.getId(), StaticTestData.meal.getId(),null);
     }
 
     @When("deleting a food item from the meal")
     public void deleting_a_food_item_from_the_meal(){
-        if (isConnectionGood) {
-            FoodAPI.deleteFoodFromMeal(user.getEmail(),metric.getId(),meal.getId(),food1.getId(), null);
+        if (StaticTestData.isConnectionGood) {
+            FoodAPI.deleteFoodFromMeal(StaticTestData.user.getEmail(), StaticTestData.metric.getId(), StaticTestData.meal.getId(), StaticTestData.food1.getId(), null);
         }
     }
 
     @When("deleting multiple food items from the meal")
     public void deleting_multiple_food_items_from_the_meal(){
-        if (isConnectionGood) {
-            FoodAPI.deleteFoodFromMeal(user.getEmail(),metric.getId(),meal.getId(),food1.getId(), null);
-            FoodAPI.deleteFoodFromMeal(user.getEmail(),metric.getId(),meal.getId(),food2.getId(), null);
+        if (StaticTestData.isConnectionGood) {
+            FoodAPI.deleteFoodFromMeal(StaticTestData.user.getEmail(), StaticTestData.metric.getId(), StaticTestData.meal.getId(), StaticTestData.food1.getId(), null);
+            FoodAPI.deleteFoodFromMeal(StaticTestData.user.getEmail(), StaticTestData.metric.getId(), StaticTestData.meal.getId(), StaticTestData.food2.getId(), null);
         }
     }
 
@@ -77,25 +39,25 @@ public class ID015_User_Deletes_Logged_Food_Item {
 
     @Then("the food item is dissociated from the meal")
     public void the_food_item_is_dissociated_from_the_meal(){
-        List<FoodDto> foods = FoodAPI.getFoodsFromMeal(user.getEmail(),metric.getId(),meal.getId(), null);
-        assert (!foods.contains(food1));
+        List<FoodDto> foods = FoodAPI.getFoodsFromMeal(StaticTestData.user.getEmail(), StaticTestData.metric.getId(), StaticTestData.meal.getId(), null);
+        assert (!foods.contains(StaticTestData.food1));
     }
 
     @Then("the food items are dissociated from the meal")
     public void the_food_items_are_dissociated_from_the_meal(){
-        List<FoodDto> foods = FoodAPI.getFoodsFromMeal(user.getEmail(),metric.getId(),meal.getId(), null);
-        assert (!foods.contains(food1) && !foods.contains(food2));
+        List<FoodDto> foods = FoodAPI.getFoodsFromMeal(StaticTestData.user.getEmail(), StaticTestData.metric.getId(), StaticTestData.meal.getId(), null);
+        assert (!foods.contains(StaticTestData.food1) && !foods.contains(StaticTestData.food2));
     }
 
     @Then("the food item is not dissociated from the meal")
     public void the_food_item_is_not_dissociated_from_the_meal(){
-        List<FoodDto> foods = FoodAPI.getFoodsFromMeal(user.getEmail(),metric.getId(),meal.getId(), null);
-        assert (foods.contains(food1));
+        List<FoodDto> foods = FoodAPI.getFoodsFromMeal(StaticTestData.user.getEmail(), StaticTestData.metric.getId(), StaticTestData.meal.getId(), null);
+        assert (foods.contains(StaticTestData.food1));
     }
 
     @And("the User has at least one logged food item for the meal")
     public void the_user_has_at_least_one_logged_food_item_for_the_meal(){
-        FoodAPI.addFoodToMeal(food1,user.getEmail(),metric.getId(),meal.getId(),null);
+        FoodAPI.addFoodToMeal(StaticTestData.food1, StaticTestData.user.getEmail(), StaticTestData.metric.getId(), StaticTestData.meal.getId(),null);
     }
 
     @And("they are indicating a food item deletion")
@@ -105,7 +67,7 @@ public class ID015_User_Deletes_Logged_Food_Item {
 
     @And("there is a lack of connection between the server and client")
     public void there_is_a_lack_of_connection_between_the_server_and_client(){
-        isConnectionGood = false;
+        StaticTestData.isConnectionGood = false;
     }
 
     @And("the Connectivity issues prevented saving message is issued")
